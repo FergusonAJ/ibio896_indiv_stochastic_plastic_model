07b8d7400fbb714677d8140ce56a7ce57cfd411c
Austin Ferguson <ferguson.austin.j@gmail.com>
2020-12-02 15:47:12 -0500

    Fix minor bugs with functions and constants

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -21,10 +21,10 @@ struct Predator{
   Predator(double x_, double y_) : x(x_), y(y_){
     a = x - y;
     if(a < -1) a = -1;
-    if(a > 1) a = 1;
+    else if(a > 1) a = 1;
     b = x + y;
     if(b < -1) b = -1;
-    if(b > 1) b = 1;
+    else if(b > 1) b = 1;
   }
 };
 
@@ -39,24 +39,22 @@ class Simulation{
   const double growth_prey = 0.9;         // r_i = Intrinsic growth rate of both prey pops
   const double birth_prey = 1.0;          // b_i = Per-capita birth rate of both prey pops
   const double death_prey = 0.1;          // d_i = Per-capita death rate of both prey pops
-  const double carrying_cap_prey = 2333;   // k_i = Carrying capacity of both prey pops
+  const double carrying_cap_prey = 2333;  // k_i = Carrying capacity of both prey pops
   const double max_attack_rate = 0.004;   // a_hat_i = Maximal attack rate of predator on prey i
   const double min_handling_time = 0.4;   // h_hat_i = Minimal handling time of prey i by predator
   const double max_conv_eff_prey_1 = 0.6; // c_hat_1 = Predator's maximal conversion efficeny of prey 1
   const double max_conv_eff_prey_2 = 0.2; // c_hat_2 = Predator's maximal conversion efficeny of prey 2
   const double x_mut_prob = 0.01;         // mu_x = Mutation probability for x (not mean)
   const double y_mut_prob = 0.01;         // mu_x = Mutation probability for y (not mean)
-  const double x_mut_var = 0.1;           // sigma_x = Variance of x mutation distribution
-  const double x_mut_std = x_mut_var * x_mut_var; // std_x = Standard dev. of x mutation distribution
-  const double y_mut_var = 0.1;           // sigma_x = Variance of x mutation distribution
-  const double y_mut_std = y_mut_var * y_mut_var; // std_y = Standard dev. of y mutation distribution
-  const double a_var = 1;                 // sigma_a = Variance of "a" distribution
-  const double a_std = a_var * a_var;     // sigma^2_a = Standard deviation of a mutation dist.
-  const double h_var = 1;                 // sigma_h = Variance of "h" distribution
-  const double h_std = h_var * h_var;     // sigma^2_h = Standard deviation of a mutation dist.
+  const double x_mut_std = 0.1;           // sigma_x = Standard deviation of x mutation distribution
+  const double y_mut_std = 0.1;           // sigma_y = Standard deviation of y mutation distribution
+  const double a_std = 1;                 // sigma_a = Standard deviation of "a" distribution
+  const double a_var = a_std * a_std;     // sigma^2_a = Variance of "a" distribution
+  const double h_std = 1;                 // sigma_h = Standard deviation of "h" distribution
+  const double h_var = h_std * h_std;     // sigma^2_h = Variance of a "h" disttribution
   
   public: 
-  double death_pred = 0.3;         // m = Per-capita death rate for predators
+  double death_pred = 0.22;         // m = Per-capita death rate for predators
   double plastic_cost = 0.1;        // k = Scaling factor for the cost of plasticity
   double habitat_sensitivity = 10;  // s = Habitat sensitivity
   size_t init_n_1 = 500;
@@ -65,21 +63,21 @@ class Simulation{
   size_t cur_n_1;
   size_t cur_n_2;
   double time_elapsed = 0;
-  double time_max = 100;
+  double time_max = 10000;
 
   protected:
 
   void UpdatePredator(Predator & pred){
     // Attack rates
     pred.attack_rates[0] = 
-        max_attack_rate * exp(-1 * ( ((-1 - pred.a) * (-1 - pred.a)) / 2 * a_var * a_var)); 
+        max_attack_rate * exp(-1.0 * ( ((-1 - pred.a) * (-1 - pred.a)) / (2 * a_std) )); 
     pred.attack_rates[1] = 
-        max_attack_rate * exp(-1 * ( ((1 - pred.b) * (1 - pred.b)) / 2 * a_var * a_var)); 
+        max_attack_rate * exp(-1.0 * ( ((1 - pred.b) * (1 - pred.b)) / (2 * a_std) )); 
     // Handling times
     pred.handling_times[0] = 
-      min_handling_time + 1 - exp(-1 * ( ((-1 - pred.a) * (-1 - pred.a)) / 2 * a_std)); 
+      min_handling_time + (1 - exp(-1.0 * ( ((-1 - pred.a) * (-1 - pred.a)) / (2 * h_std) ))); 
     pred.handling_times[1] = 
-      min_handling_time + 1 - exp(-1 * ( ((1 - pred.b) * (1 - pred.b)) / 2 * h_std)); 
+      min_handling_time + (1 - exp(-1.0 * ( ((1 - pred.b) * (1 - pred.b)) / (2 * h_std) ))); 
     // Functional responses
     pred.functional_responses[0] = 
       (pred.attack_rates[0] * cur_n_1) / (1 + pred.attack_rates[0] * pred.handling_times[0] * cur_n_1);  
@@ -89,8 +87,8 @@ class Simulation{
     pred.conv_efficiencies[0] = max_conv_eff_prey_1 * (1 - pred.y * plastic_cost);
     pred.conv_efficiencies[1] = max_conv_eff_prey_2 * (1 - pred.y * plastic_cost);
     // Profitabilities
-    pred.profitabilities[0] = pred.conv_efficiencies[0] * pred.functional_responses[0] / death_pred;  
-    pred.profitabilities[1] = pred.conv_efficiencies[1] * pred.functional_responses[1] / death_pred;  
+    pred.profitabilities[0] = (pred.conv_efficiencies[0] * pred.functional_responses[0]) / death_pred;  
+    pred.profitabilities[1] = (pred.conv_efficiencies[1] * pred.functional_responses[1]) / death_pred;  
     // Habitat selection probability
     pred.prob_hab_1 = 
         1 / (1 + exp(-1 * habitat_sensitivity * (pred.profitabilities[0] - pred.profitabilities[1])));
@@ -102,12 +100,13 @@ class Simulation{
   void FillPredatorPop(size_t num_preds){
     pred_vec.resize(num_preds);
     double x, y;
-    std::normal_distribution<double> dist(0, 0.05);
+    std::normal_distribution<double> x_dist(0, 0.05);
+    std::normal_distribution<double> y_dist(0, 0.05);
     for(size_t i = 0; i < num_preds; ++i){
-      x = dist(rand);
-      y = dist(rand);
+      x = x_dist(rand);
+      y = y_dist(rand);
       while(y < 0)
-        y = dist(rand);
+        y = y_dist(rand);
       pred_vec[i] = Predator(x,y);
     }
   }
@@ -139,7 +138,7 @@ class Simulation{
     fp_pop_size << "name,count,time" << std::endl;
     size_t update = 0;
     while(time_elapsed < time_max){
-      if(update % 50 == 0){
+      if(update % 100 == 0){
         fp_pop_size << "predator," << pred_vec.size() << "," << time_elapsed << std::endl
                     << "prey1," << cur_n_1 << "," << time_elapsed << std::endl 
                     << "prey2," << cur_n_2 << "," << time_elapsed << std::endl; 
@@ -173,6 +172,7 @@ class Simulation{
       total_death_prey_1 /= total_sum;
       total_death_prey_2 /= total_sum;
       total_death_pred /= total_sum;
+      // Let some time pass with lambda = total (total = E in paper)
       std::exponential_distribution<double> exp_dist(total_sum);
       time_elapsed += exp_dist(rand);
       // Generate random number between 0 and 1
